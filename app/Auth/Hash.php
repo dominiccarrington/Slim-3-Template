f90c62e96ebc6d9b45c178df9ca5f4414a2786ee
Dominic Carrington <carringtondominic@gmail.com>
2016-08-30 20:08:47 +0100

    Made methods static

--- app/Auth/Hash.php
+++ app/Auth/Hash.php
@@ -3,17 +3,17 @@ namespace App\Auth;
 
 class Hash
 {
-    public function create($value)
+    public static function create($value)
     {
         return password_hash($value, PASSWORD_BCRYPT);
     }
 
-    public function verify($hashedValue, $realValue)
+    public static function verify($hashedValue, $realValue)
     {
         return password_verify($realValue, $hashedValue);
     }
 
-    public function unique()
+    public static function unique()
     {
         return md5(uniqid());
     }
